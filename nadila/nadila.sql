ba7427c9483a6f1f5591d6986d43dd8df738d965
ariwardana28 <36886965+ariwardana28@users.noreply.github.com>
2020-10-07 20:43:53 +0800

    1

---
+++ nadila/nadila.sql
@@ -0,0 +1,366 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2020 at 02:26 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nadila`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$lvgoJyxFlvZ5vKkXO/jdA.fcf0q8ESvDxAvWN9v2x/3WgsJYoKNR2', '', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bayars`
+--
+
+CREATE TABLE `bayars` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_menu` bigint(20) UNSIGNED NOT NULL,
+  `meja` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sub_total` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_penjualans`
+--
+
+CREATE TABLE `detail_penjualans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_penjualan` bigint(20) UNSIGNED NOT NULL,
+  `id_menu` bigint(20) UNSIGNED NOT NULL,
+  `subtotal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `status` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_user` int(11) NOT NULL DEFAULT current_timestamp(),
+  `tanggal` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `detail_penjualans`
+--
+
+INSERT INTO `detail_penjualans` (`id`, `id_penjualan`, `id_menu`, `subtotal`, `qty`, `created_at`, `updated_at`, `status`, `id_user`, `tanggal`) VALUES
+(22, 28, 4, '63900', '3', '2020-10-02', '2020-10-02 05:01:26', 'belum bayar', 1, '2020-10-02'),
+(23, 29, 3, '42000', '2', '2020-10-05', '2020-10-04 22:55:30', 'belum bayar', 1, '2020-10-05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasirs`
+--
+
+CREATE TABLE `kasirs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kasirs`
+--
+
+INSERT INTO `kasirs` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Kasir', 'kasir@gmail.com', NULL, '$2y$10$lvgoJyxFlvZ5vKkXO/jdA.fcf0q8ESvDxAvWN9v2x/3WgsJYoKNR2', '', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `harga` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `ket` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`id`, `nama`, `harga`, `gambar`, `created_at`, `updated_at`, `ket`) VALUES
+(1, 'Es Jeruk', '12000', 'es_jeruk.jpg', '2020-09-26 05:07:20', '2020-09-26 05:07:20', NULL),
+(2, 'Kopi Cream', '15000', 'kopi_cream.jpg', '2020-09-26 06:39:25', '2020-09-26 06:39:25', ''),
+(3, 'Vietnam Drip', '21000', 'vietnam.jpg', '2020-09-26 06:39:36', '2020-09-26 06:39:36', NULL),
+(4, 'C60', '21300', 'c60.jpg', '2020-09-26 06:39:44', '2020-09-26 06:39:44', NULL),
+(5, 'Caramel Latte', '21000', 'car_latte.jpg', '2020-09-26 06:41:45', '2020-09-26 06:41:45', NULL),
+(6, 'Kopi Latte', '15000', 'kopi_lat.jpg', '2020-09-26 06:41:50', '2020-09-26 06:41:50', NULL),
+(7, 'Vanila Latte', '15000', 'vanilla.jpg', '2020-09-26 06:41:59', '2020-09-26 06:41:59', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(4, '2014_10_12_000000_create_users_table', 1),
+(5, '2014_10_12_100000_create_password_resets_table', 1),
+(6, '2020_09_09_120935_create_menus_table', 1),
+(7, '2020_09_26_123021_create_bayars_table', 1),
+(8, '2020_09_26_123103_create_penjualans_table', 1),
+(9, '2020_09_26_123120_create_detail_penjualans_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualans`
+--
+
+CREATE TABLE `penjualans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `meja` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id_user` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `penjualans`
+--
+
+INSERT INTO `penjualans` (`id`, `meja`, `tanggal`, `total`, `created_at`, `updated_at`, `id_user`, `status`) VALUES
+(28, '2', '2020-10-02', '63900', '2020-10-02 05:01:26', '2020-10-02 05:01:26', '1', 'Bayar'),
+(29, '2', '2020-10-05', '42000', '2020-10-04 22:55:30', '2020-10-04 22:55:30', '1', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Ari Wardana', 'ty@gmail.com', NULL, '$2y$10$lvgoJyxFlvZ5vKkXO/jdA.fcf0q8ESvDxAvWN9v2x/3WgsJYoKNR2', NULL, '2020-09-28 05:01:33', '2020-09-28 05:01:33'),
+(2, 'Selvi', 'sel@gmail.com', NULL, '$2y$10$GFASrGi5WU9vXDsjF82CXOJKbPZ09hUab7Qgd4VhxiqjatQacYi/m', NULL, '2020-10-01 01:31:35', '2020-10-01 01:31:35');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bayars`
+--
+ALTER TABLE `bayars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bayars_id_menu_foreign` (`id_menu`);
+
+--
+-- Indexes for table `detail_penjualans`
+--
+ALTER TABLE `detail_penjualans`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `detail_penjualans_id_penjualan_foreign` (`id_penjualan`),
+  ADD KEY `detail_penjualans_id_menu_foreign` (`id_menu`);
+
+--
+-- Indexes for table `kasirs`
+--
+ALTER TABLE `kasirs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `penjualans`
+--
+ALTER TABLE `penjualans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bayars`
+--
+ALTER TABLE `bayars`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_penjualans`
+--
+ALTER TABLE `detail_penjualans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `kasirs`
+--
+ALTER TABLE `kasirs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `penjualans`
+--
+ALTER TABLE `penjualans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bayars`
+--
+ALTER TABLE `bayars`
+  ADD CONSTRAINT `bayars_id_menu_foreign` FOREIGN KEY (`id_menu`) REFERENCES `menus` (`id`);
+
+--
+-- Constraints for table `detail_penjualans`
+--
+ALTER TABLE `detail_penjualans`
+  ADD CONSTRAINT `detail_penjualans_id_menu_foreign` FOREIGN KEY (`id_menu`) REFERENCES `menus` (`id`),
+  ADD CONSTRAINT `detail_penjualans_id_penjualan_foreign` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualans` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
